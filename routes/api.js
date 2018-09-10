45ddb1d927bb699ecbfc1b3aa9546004d2edf603
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-10 13:50:28 +0800

    Outline routes

--- routes/api.js
+++ routes/api.js
@@ -14,8 +14,16 @@ const messageHandler = require('../controllers/messageHandler')
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .post(messageHandler.addThread)
+    .get(messageHandler.listThreads)
+    .put(messageHandler.reportThread)
+    .delete(messageHandler.deleteThread)
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+    .post(messageHandler.addReply)
+    .get(messageHandler.displayThread)
+    .put(messageHandler.reportReply)
+    .delete(messageHandler.deleteReply)
 
 };
