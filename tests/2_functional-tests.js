3f182c617cb604f6272e738c43d859affea059e9
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-11 18:57:54 +0800

    Add the following test conditions:
    -Add reply to thread
    -Display thread details
    -Report thread reply

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -78,18 +78,59 @@ suite('Functional Tests', function() {
   suite('API ROUTING FOR /api/replies/:board', function() {
     
     suite('POST', function() {
-      
+      test('Add reply to thread', function(done) {
+        chai.request(server)
+          .post(`/api/replies/${TEST_BOARD}`)
+          .send({
+            _id: TEST_ID,
+            text: 'Hello, happy to be here!',
+            delete_password: 'pwd'
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200)
+            expect(res).to.redirect
+            done()
+          })
+      })
     });
     
     suite('GET', function() {
-      
+      test('Display thread details', function(done) {
+        chai.request(server)
+          .get(`/api/replies/${TEST_BOARD}`)
+          .query({thread_id: TEST_ID})
+          .end(function(err, res) {
+            console.log(`res.body: `, res.body)
+            assert.equal(res.status, 200)
+            assert.property(res.body, '_id', 'threads in array should contain _id')
+            assert.property(res.body, 'text', 'threads in array should contain text')
+            assert.property(res.body, 'created_on', 'threads in array should contain created_on field')
+            assert.property(res.body, 'bumped_on', 'threads in array should contain bumped_on field')
+            assert.isArray(res.body.replies, 'response should be an array')
+            TEST_REPLY_ID = res.body.replies[0]._id
+            done()
+          })
+      })
     });
     
     suite('PUT', function() {
-      
+      test('Report thread reply', function(done) {
+        chai.request(server)
+        .put(`/api/replies/${TEST_BOARD}`)
+        .send({
+          thread_id: TEST_ID,
+          reply_id: TEST_REPLY_ID
+        })
+        .end(function(err, res) {
+          console.log(`res.status: `, res.status)
+          console.log(`res.body: `, res.body)
+          console.log(`res.text: `, res.text)
+          assert.equal(res.status, 200)
+          assert.equal(res.text, 'success')
+          done()
+        })
+      })
     });
-    
-
     
   });
 
